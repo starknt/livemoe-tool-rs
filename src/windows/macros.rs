5def09f63a1168be173f8e411c8498bc7a186907
starknt <1431880400@qq.com>
2022-09-02 00:11:06 +0800

    chore: add lazy_static deps

--- src/windows/macros.rs
+++ src/windows/macros.rs
@@ -1,3 +1,8 @@
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+
+use winapi::{shared::minwindef::FARPROC, um::libloaderapi::{ GetProcAddress, LoadLibraryW }};
+
 #[macro_export]
 macro_rules! TEXT {
     ($x:expr) => {
@@ -11,4 +16,24 @@ macro_rules! TEXT {
             wide.as_ptr()
         }
     };
+}
+
+pub fn get_function_impl(library: &str, function: &str) -> Option<FARPROC> {
+  assert_eq!(library.chars().last(), Some('\0'));
+  assert_eq!(function.chars().last(), Some('\0'));
+
+  let module = unsafe { LoadLibraryW(TEXT!(library)) };
+  if module.is_null() {
+    return None;
+  }
+  Some(unsafe { GetProcAddress(module, function.as_ptr() as *const i8) })
+}
+
+#[macro_export]
+macro_rules! get_function {
+  ($lib:expr, $func:ident) => {
+    get_function_impl(concat!($lib, '\0'), concat!(stringify!($func), '\0')).map(|f| unsafe {
+      std::mem::transmute::<FARPROC, $func>(f)
+    })
+  };
 }
