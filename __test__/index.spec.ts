6f755e9e1996dc26f1c633d43718f75b970c3c68
starknt <1431880400@qq.com>
2022-09-02 14:44:15 +0800

    chore: test

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,29 +1,7 @@
 import { beforeAll, expect } from 'vitest'
 import { test } from 'vitest'
 
-import { hideDesktopIcon, setWindowWorker, restoreWindowWorker, setTaskbarStyle, ACCENT, showDesktopIcon, getSysListViewIconRect, setSystemCursorStyle, restoreSystemCursorStyle } from '../index'
 
 test('', () => {
-  expect(restoreWindowWorker()).toMatchInlineSnapshot('undefined')
-  expect(showDesktopIcon()).toMatchInlineSnapshot('undefined')
-  expect(setTaskbarStyle(ACCENT.AccentNormal, 0x0)).toMatchInlineSnapshot('true')
-  expect(getSysListViewIconRect()).toMatchInlineSnapshot(`
-    [
-      {
-        "bottom": 92,
-        "left": 5,
-        "right": 94,
-        "top": 0,
-      },
-      {
-        "bottom": 481,
-        "left": 374,
-        "right": 379,
-        "top": 285,
-      },
-    ]
-  `);
-
-  expect(restoreSystemCursorStyle()).toMatchInlineSnapshot('undefined')
 
 })
